d251b62451d058e7bde9f297a221b187e08e90ca
SHIMUZA <mayrontellez071097@gmail.com>
2025-07-13 04:03:08 +0000

    Actualizar gitignore para excluir archivos del sistema

--- tradeview-frontend/src/types/marketWatch.d.ts
+++
@@ -1,6 +0,0 @@
-// src/types/marketWatch.d.ts
-export interface SymbolData {
-  name: string;
-  price: number | null;
-  prevPrice: number | null;
-}
--- tradeview-frontend/src/types/symbol.d.ts
+++
@@ -1,6 +0,0 @@
-// src/types/symbol.d.ts
-interface SymbolData {
-  name: string;
-  price: number | null;
-  prevPrice: number | null;
-}
