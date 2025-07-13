3b5aca61ea6e39ff19ba8a2c1a27a4f4bc45010f
SHIMUZA <mayrontellez071097@gmail.com>
2025-07-13 03:37:47 +0000

    TradeView BackEnd

---
+++ source/repos/PruebaDesarrolloAlejandroTellezMarketWatch/PruebaDesarrolloAlejandroTellezMarketWatch/Controllers/MarketWatchController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PruebaDesarrolloAlejandroTellezMarketWatch.Data;
+using PruebaDesarrolloAlejandroTellezMarketWatch.Models;
+
+namespace PruebaDesarrolloAlejandroTellezMarketWatch.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MarketWatchController : Controller
+    {
+        private readonly AppDbContext _db;
+
+        public MarketWatchController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var symbols = _db.Symbols.Select(s => s.Name).ToList();
+            return Ok(new { symbols });
+        }
+
+        [HttpPost]
+        public IActionResult Add([FromBody] string symbolName)
+        {
+            _db.Symbols.Add(new Symbol { Name = symbolName });
+            _db.SaveChanges();
+            return Ok();
+        }
+
+        [HttpDelete("{name}")]
+        public IActionResult Delete(string name)
+        {
+            var symbol = _db.Symbols.FirstOrDefault(s => s.Name == name);
+            if (symbol == null) return NotFound();
+
+            _db.Symbols.Remove(symbol);
+            _db.SaveChanges();
+            return Ok();
+        }
+    }
+}
---
+++ source/repos/PruebaDesarrolloAlejandroTellezMarketWatch/PruebaDesarrolloAlejandroTellezMarketWatch/Models/BinanceExchangeInfo.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace PruebaDesarrolloAlejandroTellezMarketWatch.Models
+{
+    public class BinanceExchangeInfo
+    {
+        [JsonProperty("symbols")]
+        public List<BinanceSymbol> Symbols { get; set; }
+
+    }
+
+    public class BinanceSymbol
+    {
+
+        [JsonProperty("symbol")]
+        public string SymbolName { get; set; }
+
+    }
+}
