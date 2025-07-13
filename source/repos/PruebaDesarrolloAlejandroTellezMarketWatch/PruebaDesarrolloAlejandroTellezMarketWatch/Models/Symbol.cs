f72528edb5522a1c46b062570c320d9202ff428b
SHIMUZA <84953648+SHIMUZA@users.noreply.github.com>
2025-07-12 22:42:06 -0500

    Delete source/repos directory
    Eliminación por cambio de carpeta

--- source/repos/PruebaDesarrolloAlejandroTellezMarketWatch/PruebaDesarrolloAlejandroTellezMarketWatch/Models/Symbol.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace PruebaDesarrolloAlejandroTellezMarketWatch.Models
-{
-    public class Symbol
-    {
-        public int Id { get; set; }
-        public string Name { get; set; } = string.Empty;
-    }
-}
